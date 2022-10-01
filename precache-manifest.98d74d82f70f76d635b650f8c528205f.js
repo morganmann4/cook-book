d7d5ecb3b690000ec6da4a0ca0f8832ce3620889
Morgan Mann <momomann4@gmail.com>
2022-09-30 18:44:36 -0600

    Updates

---
+++ precache-manifest.98d74d82f70f76d635b650f8c528205f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159b9a5432deba8bda4216441b56c19c",
+    "url": "/cook-book/index.html"
+  },
+  {
+    "revision": "c0b357b99ce172e78723",
+    "url": "/cook-book/static/css/main.62addd90.chunk.css"
+  },
+  {
+    "revision": "dd965e47954106246d64",
+    "url": "/cook-book/static/js/2.cdfbbe4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cook-book/static/js/2.cdfbbe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b357b99ce172e78723",
+    "url": "/cook-book/static/js/main.c00e84a1.chunk.js"
+  },
+  {
+    "revision": "bd95790f750365c27d74",
+    "url": "/cook-book/static/js/runtime-main.a4e42959.js"
+  }
+]);
